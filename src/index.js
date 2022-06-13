eef404bbd8247e15a73c16319334d3ed6dac37cd
Raphael Lelis <raphalelis@outlookc.com>
2022-06-13 14:51:51 -0300

    backend lol

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const { json } = require("express")
+
+const app = express()
+
+app.use(json())
+app.listen(3333)
+
+app.get('/', async(req, res) => {
+    res.send("hello world")
+})
