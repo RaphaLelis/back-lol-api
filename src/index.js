0dfbce4f54e41d242e2ee7ec857c9c2f30e565f7
Raphael Lelis <raphalelis@outlookc.com>
2022-06-13 15:21:51 -0300

    update backend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,52 @@
-const express = require("express")
-const { json } = require("express")
+const express = require("express");
+const { json } = require("express");
+require("dotenv").config();
 
-const app = express()
+const axios = require("axios");
+const cors = require("cors");
 
-app.use(json())
-app.listen(3333)
 
-app.get('/', async(req, res) => {
-    res.send("hello world")
-})
+const app = express();
+
+app.use(json());
+app.use(cors());
+app.listen(process.env.PORT || 3333);
+
+
+app.get("/summoner/:summonerName", async(req, res) => {
+    const { summonerName } = req.params;
+    const summonerIdResponse = await axios
+        .get(
+            `${process.env.LOL_URL}/lol/summoner/v4/summoners/by-name/${encodeURI(
+        summonerName
+      )}`, { headers: { "X-Riot-Token": process.env.LOL_KEY } }
+        )
+        .catch((e) => {
+            return res.status(e.response.status).json(e.response.data);
+        });
+
+    const { id, profileIconId, summonerLevel } = summonerIdResponse.data;
+
+    const responseRanked = await axios
+        .get(`${process.env.LOL_URL}/lol/league/v4/entries/by-summoner/${id}`, {
+            headers: { "X-Riot-Token": process.env.LOL_KEY },
+        })
+        .catch((e) => {
+            return res.status(e.response.status).json(e.response.data);
+        });
+
+    const { tier, rank, wins, losses, queueType } = responseRanked.data[1] ?
+        responseRanked.data[1] :
+        responseRanked.data[0];
+
+    res.json({
+        iconUrl: `${process.env.LOL_ICONS}/${profileIconId}.png`,
+        summonerLevel,
+        tier,
+        rank,
+        wins,
+        losses,
+        queueType,
+        winRate: ((wins / (wins + losses)) * 100).toFixed(1),
+    });
+});
